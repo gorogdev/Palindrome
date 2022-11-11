b161e4d011475e8a608f7283d974c3585a860052
gorogdev <117856369+gorogdev@users.noreply.github.com>
2022-11-11 13:06:19 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+
+
+    public static void main(String[] args){
+        System.out.println("tezts");
+
+        System.out.println(Program.isPalindrome("test"));
+
+
+    }
+
+
+
+
+}
